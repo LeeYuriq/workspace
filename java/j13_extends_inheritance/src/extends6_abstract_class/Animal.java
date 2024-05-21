44230fff5abbcbd0ca1cd66c7307de9d76fbf005
LeeYuriq <0511youli@gmail.com>
2024-05-21 15:47:37 +0900

    extends0521

---
+++ java/j13_extends_inheritance/src/extends6_abstract_class/Animal.java
@@ -0,0 +1,17 @@
+package extends6_abstract_class;
+
+//추상 메소드가 하나라도 존재하면 해당 class는 추상 class로 정의되어야 함. 
+public abstract class Animal {
+
+	String kind;		//분류
+	
+	public void breath() {
+		System.out.println("공기로 호흡합니다.");
+	}
+	
+	//추상 메소드(abstract method) : 기능이 정의되어있지 않음
+	public abstract void sound();
+	
+	
+	
+}
---
+++ java/j13_extends_inheritance/src/extends6_abstract_class/Cat.java
@@ -0,0 +1,11 @@
+package extends6_abstract_class;
+
+public class Cat extends Animal{
+
+	@Override
+	public void sound() {
+		System.out.println("햐아앜");
+		
+	}
+	
+}
